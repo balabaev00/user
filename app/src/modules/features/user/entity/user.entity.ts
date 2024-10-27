94f7599c46edc5c9e2c9857901092d003406a7c3
balabaev.mm <mikhail.balabaev@dom.ru>
2024-10-27 16:42:12 +0900

    Удаление индексов

--- app/src/modules/features/user/entity/user.entity.ts
+++ app/src/modules/features/user/entity/user.entity.ts
@@ -11,12 +11,7 @@ export class UserEntity {
 }
 
 export type UserDocument = HydratedDocument<UserEntity>;
-
 export const UserSchema = SchemaFactory.createForClass(UserEntity);
-
-UserSchema.index({ storyId: 1, agreementNumber: 1 }, { unique: true });
-UserSchema.index({ endDate: 1 }, { expireAfterSeconds: 0 });
-UserSchema.index({ agreementNumber: 1 });
 
 export const UserModelDefinition = {
     name: UserEntity.name,
